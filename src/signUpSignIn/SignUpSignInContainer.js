29e2805c3e91f462f582dedc309e4d17a287b6c6
fred <fred.zhang.fz@gmail.com>
2017-12-10 21:23:30 +1300

    Added signUpSignIn

---
+++ src/signUpSignIn/SignUpSignInContainer.js
@@ -0,0 +1,72 @@
+// @flow
+
+import * as userAccessActions from 'micro-business-parse-server-common-react-native/src/userAccess/Actions';
+import {
+  UserAccessStatus
+} from 'micro-business-parse-server-common-react-native';
+import React, {
+  Component
+} from 'react';
+import PropTypes from 'prop-types';
+import {
+  bindActionCreators
+} from 'redux';
+import {
+  connect
+} from 'react-redux';
+import SignUpSignIn from './SignUpSignIn';
+
+class UserSignInSignUpContainer extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  onSignInWithFacebookClicked = () => {
+    this.props.userAccessActions.signInWithFacebook('public_profile,email', 'individual');
+  };
+
+  onSignInClicked = (emailAddress, password) => {
+    this.props.userAccessActions.signInWithUsernameAndPassword(emailAddress, password);
+  };
+
+  onSignUpClicked = (emailAddress, password) => {
+    this.props.userAccessActions.signUpWithUsernameAndPassword(emailAddress, password, emailAddress, 'individual');
+  };
+
+  render = () => {
+    return (
+      <SignUpSignIn
+        onSignInWithFacebookClicked={this.onSignInWithFacebookClicked}
+        onSignInClicked={this.onSignInClicked}
+        onSignUpClicked={this.onSignUpClicked}
+        signUpOrSignInIsInProgress={
+          this.props.signInStatus === UserAccessStatus.IN_PROGRESS || this.props.signUpStatus === UserAccessStatus.IN_PROGRESS
+        }
+        title={this.props.title}
+      />
+    );
+  };
+}
+
+UserSignInSignUpContainer.propTypes = {
+  userAccessActions: PropTypes.object.isRequired,
+  signInStatus: PropTypes.number.isRequired,
+  signUpStatus: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+function mapStateToProps(state, props) {
+  return {
+    signInStatus: state.userAccess.get('signInStatus'),
+    signUpStatus: state.userAccess.get('signUpStatus'),
+    title: props.title,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    userAccessActions: bindActionCreators(userAccessActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserSignInSignUpContainer);
---
+++ src/signUpSignIn/Styles.js
@@ -0,0 +1,37 @@
+// @flow
+
+import {
+  StyleSheet
+} from 'react-native';
+
+export default StyleSheet.create({
+  activityIndicator: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  topContainer: {
+    paddingTop: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  grid: {
+    alignItems: 'center',
+  },
+  title: {
+    color: '#3b5998',
+  },
+  button: {
+    marginTop: 15,
+    height: 50,
+  },
+  signInButtonContainerViewStyle: {
+    borderRadius: 35,
+  },
+  signUpButtonContainerViewStyle: {
+    borderRadius: 35,
+  },
+});
